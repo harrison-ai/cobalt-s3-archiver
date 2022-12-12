f1650f853f9d045abe6c0eb7bb0b3ec84417212d
Oliver Daff <oliverdaff@users.noreply.github.com>
2022-12-12 22:17:00 +1000

    Added docs for ZipEntries.

--- src/list.rs
+++ src/list.rs
@@ -6,10 +6,12 @@ use cobalt_aws::s3::S3Object;
 use crate::aws::S3ObjectSeekableRead;
 
 /// Provide a method of iterating over the details in of archived
-/// files without
+/// files without reading the entire archive.
 pub struct ZipEntries<'a>(ZipFileReader<S3ObjectSeekableRead<'a>>);
 
 impl<'a> ZipEntries<'a> {
+
+    ///Create a new `ZipEntries` for the `src` object.
     pub async fn new<'b>(
         client: &'b aws_sdk_s3::Client,
         src: &'b S3Object,
@@ -20,6 +22,7 @@ impl<'a> ZipEntries<'a> {
         Ok(ZipEntries(zip_reader))
     }
 
+    /// Return an `IntoIterator` over the entries in the archive file.
     pub fn entries(&'a self) -> impl IntoIterator<Item = &'a ZipEntry> {
         self.0.entries().into_iter()
     }
